11661fec584dc59d5086dad7b4f8414ea5bce741
GuillermoMGodoy <125904939+GuillermoMGodoy@users.noreply.github.com>
2025-06-19 23:10:04 -0300

    Modificacion de la funcion getAllImages()
    Llamamos API y convertimos  los JSON  en objetos Card

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -8,11 +8,14 @@ from django.contrib.auth import get_user
 
 # función que devuelve un listado de cards. Cada card representa una imagen de la API de Pokemon
 def getAllImages():
+    raw_images = transport.getAllImages()
+    cards = [translator.fromRequestIntoCard(raw) for raw in raw_images]
+    return cards
     # debe ejecutar los siguientes pasos:
     # 1) traer un listado de imágenes crudas desde la API (ver transport.py)
     # 2) convertir cada img. en una card.
     # 3) añadirlas a un nuevo listado que, finalmente, se retornará con todas las card encontradas.
-    pass
+
 
 # función que filtra según el nombre del pokemon.
 def filterByCharacter(name):
